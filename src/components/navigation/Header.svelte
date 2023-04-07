37f473966b8668fcec00ad25cce38bbe48857861
DiegoRascon <joserascon2002@gmail.com>
2023-04-07 16:52:00 -0700

    Se agregó un componente de encabezado

---
+++ src/components/navigation/Header.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Icon from '@iconify/svelte';
+	import SectionTitle from '../SectionTitle.svelte';
+</script>
+
+<div class="flex justify-between p-4 bg-stone-900 md:hidden">
+	<button class="p-2 hover:bg-stone-800 active:bg-stone-950 rounded-xl transition-all">
+		<Icon icon="ion:menu" height={24} />
+	</button>
+	<SectionTitle text="Inicio" />
+</div>
