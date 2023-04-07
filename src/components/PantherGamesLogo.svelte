8c0c565286ea453e7d45ee444a4e4b3673cc236a
DiegoRascon <joserascon2002@gmail.com>
2023-04-07 13:11:52 -0700

    Se creó el componente PantherGamesLogo.svelte

---
+++ src/components/PantherGamesLogo.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let size: Number;
+</script>
+
+<div class="text-center -space-y-2 text-{size}xl font-lexend text-pink-700">
+	<h1 class="font-bold">panther</h1>
+	<h1>games</h1>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,12 @@
 <script>
+	import PantherGamesLogo from '../components/PantherGamesLogo.svelte';
 	import SignInInput from '../components/SignInInput.svelte';
 </script>
 
-<div class="relative flex flex-col min-h-screen justify-center overflow-hidden bg-stone-900">
-	<div class="relative mx-auto p-8 bg-stone-950 border border-stone-800 rounded-xl shadow-xl">
-		<form class="flex flex-col space-y-4">
-			<div class="text-center text-4xl font-lexend text-pink-700">
-				<h1 class="font-bold">Panther</h1>
-				<h1>Games</h1>
-			</div>
+<div class="relative flex flex-col min-h-screen justify-center bg-stone-900">
+	<div class="mx-auto p-8 bg-stone-950 border border-stone-800 rounded-xl shadow-xl">
+		<form class="flex flex-col w-64 space-y-4">
+			<PantherGamesLogo size={4} />
 			<SignInInput type="text" placeholder="Usuario" />
 			<SignInInput type="password" placeholder="Contraseña" />
 			<a class="px-4 py-2 rounded-xl text-center font-bold btn-fill" href="/home">Iniciar sesión</a>
