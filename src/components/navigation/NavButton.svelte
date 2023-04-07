36175a0d758d7e9ef63ef83e477c1c3fac7b3cca
DiegoRascon <joserascon2002@gmail.com>
2023-04-07 14:10:57 -0700

    Se agregó el funcionamiento de íconos en los botones de la barra de navegación lateral

--- src/components/navigation/NavButton.svelte
+++ src/components/navigation/NavButton.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
-    export let href: string;
-    export let text: string
+	import Icon from '@iconify/svelte';
+	export let href: string;
+	export let text: string;
+    export let icon: string;
 </script>
 
-<a {href} class="">{text}</a>
+<a {href} class="flex p-4 hover:bg-stone-800 rounded-xl"
+	><Icon icon={icon} width={24} />{text}</a
+>
--- src/components/navigation/Navbar.svelte
+++ src/components/navigation/Navbar.svelte
@@ -6,10 +6,10 @@
 <div class="w-64 p-4 bg-stone-900 border-r border-stone-800 shadow-xl">
 	<nav class="flex flex-col">
 		<a href="/home"><PantherGamesLogo size={2} /></a>
-		<NavButton href="/home" text="Inicio" />
-		<NavButton href="/home/products" text="Productos" />
-		<NavButton href="/home/sales" text="Ventas" />
-		<NavButton href="/home/reports" text="Reportes" />
-		<NavButton href="/" text="Cerrar sesión" />
+		<NavButton href="/home" text="Inicio" icon="mdi-light:home" />
+		<NavButton href="/home/products" text="Productos" icon="mdi-light:home" />
+		<NavButton href="/home/sales" text="Ventas" icon="mdi-light:home" />
+		<NavButton href="/home/reports" text="Reportes" icon="mdi-light:home" />
+		<NavButton href="/" text="Cerrar sesión" icon="mdi-light:home" />
 	</nav>
 </div>
