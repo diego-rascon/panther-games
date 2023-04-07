d34abf76629ce1c68646777b5a63e4aaed710478
DiegoRascon <joserascon2002@gmail.com>
2023-04-07 16:48:07 -0700

    Ahora se organizan los elementos verticalmente

--- src/components/PantherGamesLogo.svelte
+++ src/components/PantherGamesLogo.svelte
@@ -2,7 +2,7 @@
 	export let size: Number;
 </script>
 
-<div class="text-center -space-y-2 text-{size}xl font-lexend text-pink-700">
+<div class="flex flex-col text-center -space-y-2 text-{size}xl font-lexend text-pink-700 select-none">
 	<h1 class="font-bold">panther</h1>
 	<h1>games</h1>
 </div>
