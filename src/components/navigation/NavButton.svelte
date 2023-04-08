857278ad8234e6c62d74e866f2018dde8442ed9e
DiegoRascon <joserascon2002@gmail.com>
2023-04-07 21:57:32 -0700

    Ahora resalta el botón de la sección actual

--- src/components/navigation/NavButton.svelte
+++ src/components/navigation/NavButton.svelte
@@ -1,12 +1,26 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import Icon from '@iconify/svelte';
 	export let href: string;
 	export let text: string;
 	export let icon: string;
+
+	let isActive: boolean = false;
+
+	$: {
+		isActive = false;
+		const route: string | null = $page.route.id;
+		if (href === route) isActive = true;
+	}
 </script>
 
-<a {href}>
-	<div class="flex p-4 space-x-4 hover:bg-stone-800 active:bg-stone-950 rounded-xl transition-all select-none">
+<a
+	{href}
+	class="p-4 rounded-xl outline-none transition-all select-none {isActive
+		? 'btn-fill font-bold'
+		: 'hover:bg-stone-800 active:bg-stone-950 outline-none focus:outline-pink-700 selection:outline-none'}"
+>
+	<div class="flex space-x-4">
 		<Icon {icon} height={24} />
 		<p>{text}</p>
 	</div>
