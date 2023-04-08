2c13368a2aba4edeb192f402eb24adfff4238387
DiegoRascon <joserascon2002@gmail.com>
2023-04-07 21:21:06 -0700

    Se implementó la barra de navegación estatica

--- src/components/navigation/Navbar.svelte
+++ src/components/navigation/Navbar.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	import PantherGamesLogo from '../PantherGamesLogo.svelte';
 	import NavButton from './NavButton.svelte';
-	import { sections } from './sectionsModel';
+	import { sections } from '../sectionsModel';
 </script>
 
 <div
-	class="absolute inset-y-0 left-0 w-64 p-4 transform -translate-x-full md:relative md:translate-x-0 bg-stone-900 border-r border-stone-800 shadow-xl transition-all"
+	class="fixed inset-y-0 left-0 w-64 p-4 bg-stone-900 border-r border-stone-800 shadow-xl transition-all"
 >
 	<nav class="flex flex-col">
 		<a href="/home" class="pb-4"><PantherGamesLogo size={2} /></a>
