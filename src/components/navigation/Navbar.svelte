05c4263492abd24f93094b956ef3a98d56fe9097
DiegoRascon <joserascon2002@gmail.com>
2023-04-07 19:40:38 -0700

    Se implementó una lista de objetos que contiene todas las secciones de la navbar

--- src/components/navigation/Navbar.svelte
+++ src/components/navigation/Navbar.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
 	import PantherGamesLogo from '../PantherGamesLogo.svelte';
 	import NavButton from './NavButton.svelte';
+	import { sections } from './sectionsModel';
 </script>
 
 <div
-	class="absolute inset-y-0 left-0 w-64 p-4 transform -translate-x-full md:relative md:translate-x-0 bg-stone-900 border-r border-stone-800 shadow-xl transition"
+	class="absolute inset-y-0 left-0 w-64 p-4 transform -translate-x-full md:relative md:translate-x-0 bg-stone-900 border-r border-stone-800 shadow-xl transition-all"
 >
 	<nav class="flex flex-col">
 		<a href="/home" class="pb-4"><PantherGamesLogo size={2} /></a>
-		<NavButton href="/home" text="Inicio" icon="ion:home-outline" />
-		<NavButton href="/home/products" text="Productos" icon="ion:game-controller-outline" />
-		<NavButton href="/home/sales" text="Ventas" icon="mdi-light:home" />
-		<NavButton href="/home/reports" text="Reportes" icon="mdi-light:home" />
-		<NavButton href="/" text="Cerrar sesión" icon="mdi-light:home" />
+		{#each sections as section}
+			<NavButton href={section.href} text={section.text} icon={section.icon} />
+		{/each}
 	</nav>
 </div>
