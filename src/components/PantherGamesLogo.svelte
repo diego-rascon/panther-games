7b382affd2210d23150b83ae5d7dbf6b116954ca
DiegoRascon <joserascon2002@gmail.com>
2023-04-07 21:18:26 -0700

    Se agregaron efectos de color al logo de Panther Games

--- src/components/PantherGamesLogo.svelte
+++ src/components/PantherGamesLogo.svelte
@@ -2,7 +2,7 @@
 	export let size: Number;
 </script>
 
-<div class="flex flex-col text-center -space-y-2 text-{size}xl font-lexend text-pink-700 select-none">
+<div class="flex flex-col text-center -space-y-2 text-{size}xl font-lexend text-pink-700 hover:text-pink-600 active:text-pink-800 select-none transition-all">
 	<h1 class="font-bold">panther</h1>
 	<h1>games</h1>
 </div>
