87c79afc56f57cf70811f466309187fbefe2d9d8
DiegoRascon <joserascon2002@gmail.com>
2023-04-07 21:55:35 -0700

    Se modificó el espaciado del título

--- src/components/navigation/Navbar.svelte
+++ src/components/navigation/Navbar.svelte
@@ -5,10 +5,10 @@
 </script>
 
 <div
-	class="fixed inset-y-0 left-0 w-64 p-4 bg-stone-900 border-r border-stone-800 shadow-xl transition-all"
+	class="fixed inset-y-0 left-0 w-64 p-4 pt-0 bg-stone-900 border-r border-stone-800 shadow-xl transition-all"
 >
 	<nav class="flex flex-col">
-		<a href="/home" class="pb-4"><PantherGamesLogo size={2} /></a>
+		<a href="/home" class="p-4 outline-none focus:outline-pink-700 rounded-xl"><PantherGamesLogo size={2} /></a>
 		{#each sections as section}
 			<NavButton href={section.href} text={section.text} icon={section.icon} />
 		{/each}
