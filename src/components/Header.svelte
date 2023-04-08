228caf6d5bb1ab99d4582cd224d6571bd521f5a0
DiegoRascon <joserascon2002@gmail.com>
2023-04-07 21:19:02 -0700

    Se agregó el componente Header que va a contener el título de la sección y la barra de búsqueda

---
+++ src/components/Header.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import SectionTitle from './SectionTitle.svelte';
+	import { sections, type sectionData } from './sectionsModel';
+
+	let title: string;
+
+	$: {
+		const route: string | null = $page.route.id;
+		const section: sectionData | undefined = sections.find((section) => section.href === route);
+		if (section) title = section.text;
+	}
+</script>
+
+<div class="flex items-center space-x-4">
+	<SectionTitle text={title} />
+</div>
