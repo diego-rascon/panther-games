44c8587a483028f2f9c47a0c161ffee68ee0be8a
DiegoRascon <joserascon2002@gmail.com>
2023-04-10 13:08:35 -0700

    Se agregó animación

--- src/components/ShoppingCart.svelte
+++ src/components/ShoppingCart.svelte
@@ -1,7 +1,13 @@
-<script>
+<script lang="ts">
 	import SectionTitle from './titles/SectionTitle.svelte';
+
+	export let cartVisible: boolean;
 </script>
 
-<div class="fixed inset-y-0 right-0 w-64 p-4 bg-stone-900 border-l border-stone-800 transition">
+<div
+	class="fixed inset-y-0 right-0 w-64 p-4 bg-stone-900 border-l border-stone-800 transition ease-in-out {!cartVisible
+		? 'translate-x-full'
+		: ''}"
+>
 	<SectionTitle text="Carrito" />
 </div>
