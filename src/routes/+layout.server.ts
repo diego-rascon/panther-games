10c40e2c5269ab5f26f8be2cc2757d199ad3ae22
DiegoRascon <joserascon2002@gmail.com>
2023-04-09 18:13:32 -0700

    Se pasa la sesión en el root layout

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,6 +2,6 @@ import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ locals: { getSession } }) => {
 	return {
-		session: getSession()
+		session: await getSession()
 	};
 };
