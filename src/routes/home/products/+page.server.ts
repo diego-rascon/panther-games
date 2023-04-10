462dfc31f125ec90560fbd77a24f5322702c1856
DiegoRascon <joserascon2002@gmail.com>
2023-04-10 13:08:49 -0700

    Se eliminó el archivo que maneja el lado del servidor

--- src/routes/home/products/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { supabase } from '../../../lib/supabaseClient';
-
-export async function load() {
-	const { data } = await supabase.from('categories').select();
-	return {
-		categories: data ?? []
-	};
-}
