37074f5f5e2dc087f0e418a23ba4eacca501d577
DiegoRascon <joserascon2002@gmail.com>
2023-04-09 22:30:16 -0700

    Componente para realizar las ventas

---
+++ src/components/ShoppingCart.svelte
@@ -0,0 +1,7 @@
+<script>
+	import SectionTitle from './titles/SectionTitle.svelte';
+</script>
+
+<div class="fixed inset-y-0 right-0 w-64 p-4 bg-stone-900 border-l border-stone-800 transition">
+	<SectionTitle text="Carrito" />
+</div>
