547d4490578fdefe1f0797b92da888a10ea70902
DiegoRascon <joserascon2002@gmail.com>
2023-04-09 22:30:54 -0700

    Se agregaron los botones de categorías, de agregar productos y la sección para realizar ventas

--- src/routes/home/products/+page.svelte
+++ src/routes/home/products/+page.svelte
@@ -1,14 +1,31 @@
-<script>
+<script lang="ts">
+	import AddButton from '../../../components/AddButton.svelte';
+	import Category from '../../../components/Category.svelte';
+	import ShoppingCart from '../../../components/ShoppingCart.svelte';
 	import SectionSubtitle from '../../../components/titles/SectionSubtitle.svelte';
 
 	export let data;
 	let { categories } = data;
 	$: ({ categories } = data);
+
+	let cartVisible: boolean = true;
+
+	function setCartVisible() {
+		cartVisible = !cartVisible;
+	}
 </script>
 
-<SectionSubtitle text="Categorías" />
-<ul>
-	{#each categories as category}
-		<li>{category.name}</li>
-	{/each}
-</ul>
+<div class="transition-all {cartVisible ? 'mr-64' : ''}">
+	<SectionSubtitle text="Categorías" />
+	<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 transition-all">
+		{#each categories as category}
+			<Category text={category.name} />
+		{/each}
+	</div>
+	<SectionSubtitle text="Inventario" />
+	<p>hashdas</p>
+	{#if cartVisible}
+		<ShoppingCart />
+	{/if}
+	<AddButton {cartVisible} buttonHandler={setCartVisible} />
+</div>
