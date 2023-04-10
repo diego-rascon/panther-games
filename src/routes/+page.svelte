7600909cc1097e39991fb4ddd5d6e91eaaee50c0
DiegoRascon <joserascon2002@gmail.com>
2023-04-09 21:06:47 -0700

    Ahora se usa botón de tipo "submit" en vez de un link normal

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,11 +5,11 @@
 
 <div class="relative flex flex-col min-h-screen justify-center bg-stone-900">
 	<div class="mx-auto p-8 bg-stone-950 border border-stone-800 rounded-xl shadow-xl">
-		<form class="flex flex-col w-64 space-y-4">
+		<form action="/home" method="post" class="flex flex-col w-64 space-y-4">
 			<PantherGamesLogo size={4} />
 			<SignInInput type="text" placeholder="Usuario" />
 			<SignInInput type="password" placeholder="Contraseña" />
-			<a class="px-4 py-2 rounded-xl text-center font-bold btn-fill" href="/home">Iniciar sesión</a>
+			<button type="submit" class="px-4 py-2 rounded-xl font-bold btn-fill">Iniciar sesión</button>
 		</form>
 	</div>
 </div>
