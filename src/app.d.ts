bbf83c43008db2ef06870c0431355938b0d62c2b
DiegoRascon <joserascon2002@gmail.com>
2023-04-09 18:17:20 -0700

    Se agregaron los tipos de Supabase

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,10 @@
 import { SupabaseClient, Session } from '@supabase/supabase-js';
+import { Database } from './DatabaseDefinitions';
 
 declare global {
 	namespace App {
 		interface Locals {
-			supabase: SupabaseClient;
+			supabase: SupabaseClient<Database>;
 			getSession(): Promise<Session | null>;
 		}
 		interface PageData {
