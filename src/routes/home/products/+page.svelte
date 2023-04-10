45c7f0cf8dfcd21a7a476405f606b9d34cf8e1fa
DiegoRascon <joserascon2002@gmail.com>
2023-04-09 20:58:37 -0700

    Ahora se muestran las categorías de la base de datos

--- src/routes/home/products/+page.svelte
+++ src/routes/home/products/+page.svelte
@@ -1,5 +1,14 @@
 <script>
 	import SectionSubtitle from '../../../components/titles/SectionSubtitle.svelte';
+
+	export let data;
+	let { countries } = data;
+	$: ({ countries } = data);
 </script>
 
 <SectionSubtitle text="Categorías" />
+<ul>
+	{#each countries as country}
+		<li>{country.name}</li>
+	{/each}
+</ul>
