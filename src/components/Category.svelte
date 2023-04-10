65f3ebb7fa5c7b6efa90211fd23b201327628430
DiegoRascon <joserascon2002@gmail.com>
2023-04-09 22:29:57 -0700

    Se agregó un componente para los botones de las categorías

---
+++ src/components/Category.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	export let text: string;
+</script>
+
+<button class="p-4 outline outline-pink-700 hover:bg-stone-900 active:bg-black rounded-xl transition-all">
+    <p class="select-none font-lexend font-bold">{text}</p>
+</button>
