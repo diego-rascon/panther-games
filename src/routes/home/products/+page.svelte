93e047a782094efc3f5040e4fac3c0df1b7cbbdd
DiegoRascon <joserascon2002@gmail.com>
2023-04-11 13:45:12 -0700

    Ahora se muestran los productos de la base de datos y se guardans las plataformas y los productos con plataformas

--- src/routes/home/products/+page.svelte
+++ src/routes/home/products/+page.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
-	import { getContext } from 'svelte';
 	import AddButton from '../../../components/AddButton.svelte';
 	import Category from '../../../components/Category.svelte';
+	import Product from '../../../components/Product.svelte';
 	import ShoppingCart from '../../../components/ShoppingCart.svelte';
 	import SectionSubtitle from '../../../components/titles/SectionSubtitle.svelte';
 
 	let cartVisible: boolean = false;
 
 	export let data;
-	let { categories } = data;
-	$: ({ categories } = data);
+	let { categories, products, platforms, productsPlatforms } = data;
+	$: ({ categories, products, platforms, productsPlatforms } = data);
 
 	function setCartVisible() {
 		cartVisible = !cartVisible;
@@ -19,12 +19,16 @@
 <div class="transition-all {cartVisible ? 'mr-64' : ''}">
 	<SectionSubtitle text="Categorías" />
 	<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 transition-all">
-		{#each data.categories as category}
-			<Category text={category.name} />
+		{#each categories as category}
+			<Category text={category.categoria_nombre} />
 		{/each}
 	</div>
 	<SectionSubtitle text="Inventario" />
-	<p>hashdas</p>
+	<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 transition-all">
+		{#each products as product}
+			<Product text={product.producto_nombre} category={product.categoria_id} />
+		{/each}
+	</div>
 	<ShoppingCart {cartVisible} />
 	<AddButton {cartVisible} buttonHandler={setCartVisible} />
 </div>
