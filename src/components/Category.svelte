8fcafb1bf631c4c5239d7e1946e768c6777200fe
DiegoRascon <joserascon2002@gmail.com>
2023-04-11 13:11:48 -0700

    Se reformateó el código

--- src/components/Category.svelte
+++ src/components/Category.svelte
@@ -2,6 +2,8 @@
 	export let text: string;
 </script>
 
-<button class="p-4 border-2 border-pink-700 hover:bg-stone-900 active:bg-black outline-none focus:outline-pink-700 rounded-xl transition-all">
-    <p class="select-none font-lexend font-bold">{text}</p>
+<button
+	class="p-4 border-2 hover:bg-stone-900 active:bg-black border-pink-700 outline-none focus:outline-pink-700 rounded-xl transition-all"
+>
+	<p class="select-none font-lexend font-bold">{text}</p>
 </button>
