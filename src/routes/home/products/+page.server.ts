0cb00ee1dfce927b6279236d619b74549bfd2e5e
DiegoRascon <joserascon2002@gmail.com>
2023-04-11 13:41:00 -0700

    Ahora también se cargan las plataformas y los productos con plataforma

--- src/routes/home/products/+page.server.ts
+++ src/routes/home/products/+page.server.ts
@@ -3,9 +3,13 @@ import { supabase } from '$lib/supabaseClient';
 export async function load() {
 	const { data: categories } = await supabase.from('categorias').select();
 	const { data: products } = await supabase.from('productos').select();
+	const { data: platforms } = await supabase.from('plataformas').select();
+	const { data: productsPlatforms } = await supabase.from('productos_plataformas').select();
 
 	return {
 		categories: categories ?? [],
-		products: products ?? []
+		products: products ?? [],
+		platforms: platforms ?? [],
+		productsPlatforms: productsPlatforms ?? []
 	};
 }
