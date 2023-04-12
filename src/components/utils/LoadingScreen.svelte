d336c1b1982ecc001c46f0316c4371ca168537e7
DiegoRascon <joserascon2002@gmail.com>
2023-04-12 15:01:44 -0700

    Se creó una carpeta para las pantallas de carga y de error

---
+++ src/components/utils/LoadingScreen.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+
+	export let text: string;
+</script>
+
+<div class="flex items-center justify-center py-4 space-x-2 transition-all">
+	<Icon class="animate-spin" icon="mingcute:loading-3-fill" height={28} />
+	<p>Cargando {text}...</p>
+</div>
