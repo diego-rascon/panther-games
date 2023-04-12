f81c381e9e2cacc208b927dc1ba0b3addf9ca8e7
DiegoRascon <joserascon2002@gmail.com>
2023-04-11 23:16:45 -0700

    Ahora se muestra una pantalla de carga y una pantalla de errores cuando se cargan los usuarios

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,27 @@
-<script>
+<script lang="ts">
+	import ErrorScreen from '../components/ErrorScreen.svelte';
 	import SignInInput from '../components/inputs/SignInInput.svelte';
+	import LoadingScreen from '../components/LoadingScreen.svelte';
 	import PantherGamesLogo from '../components/PantherGamesLogo.svelte';
+
+	export let data;
+	$:({ users } = data.streamed);
 </script>
 
-<div class="relative flex flex-col min-h-screen justify-center bg-stone-900">
-	<div class="mx-auto p-8 bg-stone-950 border border-stone-800 rounded-xl shadow-xl">
-		<form action="/home" class="flex flex-col w-64 space-y-4">
-			<PantherGamesLogo size={4} />
-			<SignInInput type="text" placeholder="Usuario" />
-			<SignInInput type="password" placeholder="Contraseña" />
-			<button type="submit" class="px-4 py-2 rounded-xl font-bold btn-fill">Iniciar sesión</button>
-		</form>
+{#await data.streamed.users}
+	<LoadingScreen />
+{:then users}
+	<div class="relative flex flex-col min-h-screen justify-center bg-stone-900">
+		<div class="mx-auto p-8 bg-stone-950 border border-stone-800 rounded-xl shadow-xl">
+			<form action="/home" class="flex flex-col w-64 space-y-4">
+				<PantherGamesLogo size={4} />
+				<SignInInput type="text" placeholder="Usuario" />
+				<SignInInput type="password" placeholder="Contraseña" />
+				<button type="submit" class="px-4 py-2 rounded-xl font-bold btn-fill">Iniciar sesión</button
+				>
+			</form>
+		</div>
 	</div>
-</div>
+{:catch error}
+	<ErrorScreen error={error.message} />
+{/await}
