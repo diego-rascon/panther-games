ff35c12d7b1f8cfa59d756d6f08f1b5e82964f3d
DiegoRascon <joserascon2002@gmail.com>
2023-04-12 17:42:53 -0700

    Se removieron las clases inútiles

--- src/routes/home/+layout.svelte
+++ src/routes/home/+layout.svelte
@@ -3,7 +3,7 @@
 	import Navbar from '../../components/navigation/Navbar.svelte';
 </script>
 
-<div class="relative min-h-screen md:flex bg-stone-950">
+<div class="min-h-screen bg-stone-950">
 	<Navbar />
 	<div class="flex-1 p-4 ml-64">
 		<Header />
