d0374f39e8758e62256d055ba9bae8f0078f3f99
DiegoRascon <joserascon2002@gmail.com>
2023-04-13 11:20:53 -0700

    Se eliminó la ruta de productos, ahora esa es la pantalla principal

--- src/models/sectionsModel.ts
+++ src/models/sectionsModel.ts
@@ -2,12 +2,12 @@ export type sectionData = {
 	href: string;
 	text: string;
 	icon: string;
-}
+};
 
 export const sections: sectionData[] = [
-	{ href: '/home', text: 'Inicio', icon: 'solar:home-smile-outline' },
-	{ href: '/home/products', text: 'Productos', icon: 'solar:gamepad-outline' },
+	{ href: '/home', text: 'Productos', icon: 'solar:gamepad-outline' },
 	{ href: '/home/sales', text: 'Ventas', icon: 'solar:dollar-minimalistic-outline' },
+	{ href: '/home/members', text: 'Miembros', icon: 'solar:crown-star-outline' },
 	{ href: '/home/reports', text: 'Reportes', icon: 'solar:document-text-outline' },
-	{ href: '/', text: 'Cerrar sesión', icon: 'solar:logout-outline' },
+	{ href: '/', text: 'Cerrar sesión', icon: 'solar:logout-outline' }
 ];
