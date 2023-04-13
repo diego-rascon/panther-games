8e5eed0217cb3d779492233faec1d73e088323e3
DiegoRascon <joserascon2002@gmail.com>
2023-04-13 16:51:30 -0700

    Se agregó la ruta de clientes

--- src/models/sectionsModel.ts
+++ src/models/sectionsModel.ts
@@ -8,6 +8,7 @@ export const sections: sectionData[] = [
 	{ href: '/home', text: 'Productos', icon: 'solar:gamepad-outline' },
 	{ href: '/home/sales', text: 'Ventas', icon: 'solar:dollar-minimalistic-outline' },
 	{ href: '/home/members', text: 'Miembros', icon: 'solar:crown-star-outline' },
+	{ href: '/home/clients', text: 'Clientes', icon: 'solar:user-outline' },
 	{ href: '/home/reports', text: 'Reportes', icon: 'solar:document-text-outline' },
 	{ href: '/', text: 'Cerrar sesión', icon: 'solar:logout-outline' }
 ];
