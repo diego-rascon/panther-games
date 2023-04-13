f8642754ebe6444461b7f63b89e1ef4c7dfeec72
DiegoRascon <joserascon2002@gmail.com>
2023-04-12 17:45:23 -0700

    Se le agregó padding al div

--- src/components/utils/LoadingScreen.svelte
+++ src/components/utils/LoadingScreen.svelte
@@ -4,7 +4,7 @@
 	export let text: string;
 </script>
 
-<div class="flex items-center justify-center py-4 space-x-2 transition-all">
+<div class="flex items-center justify-center p-4 space-x-2 transition-all">
 	<Icon class="animate-spin" icon="mingcute:loading-3-fill" height={28} />
 	<p>Cargando {text}...</p>
 </div>
