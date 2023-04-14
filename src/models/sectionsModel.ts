e1b7643507e9e8a0e7c977efa96297f655ce7dfd
DiegoRascon <joserascon2002@gmail.com>
2023-04-13 17:30:02 -0700

    Ahora icon es de tipo IconifyIcon

--- src/models/sectionsModel.ts
+++ src/models/sectionsModel.ts
@@ -1,14 +1,22 @@
+import type { IconifyIcon } from '@iconify/types';
+import gamepadOutline from '@iconify/icons-solar/gamepad-outline';
+import dollarMinimalisticOutline from '@iconify/icons-solar/dollar-minimalistic-outline';
+import crownStarOutline from '@iconify/icons-solar/crown-star-outline';
+import userOutline from '@iconify/icons-solar/user-outline';
+import documentTextOutline from '@iconify/icons-solar/document-text-outline';
+import logoutOutline from '@iconify/icons-solar/logout-outline';
+
 export type sectionData = {
 	href: string;
 	text: string;
-	icon: string;
+	icon: IconifyIcon;
 };
 
 export const sections: sectionData[] = [
-	{ href: '/home', text: 'Productos', icon: 'solar:gamepad-outline' },
-	{ href: '/home/sales', text: 'Ventas', icon: 'solar:dollar-minimalistic-outline' },
-	{ href: '/home/members', text: 'Miembros', icon: 'solar:crown-star-outline' },
-	{ href: '/home/clients', text: 'Clientes', icon: 'solar:user-outline' },
-	{ href: '/home/reports', text: 'Reportes', icon: 'solar:document-text-outline' },
-	{ href: '/', text: 'Cerrar sesión', icon: 'solar:logout-outline' }
+	{ href: '/home', text: 'Productos', icon: gamepadOutline },
+	{ href: '/home/sales', text: 'Ventas', icon: dollarMinimalisticOutline },
+	{ href: '/home/members', text: 'Miembros', icon: crownStarOutline },
+	{ href: '/home/clients', text: 'Clientes', icon: userOutline },
+	{ href: '/home/reports', text: 'Reportes', icon: documentTextOutline },
+	{ href: '/', text: 'Cerrar sesión', icon: logoutOutline }
 ];
