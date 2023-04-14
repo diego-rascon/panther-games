d4a34b59c2cfd022dde47319932fb79f9b74446a
DiegoRascon <joserascon2002@gmail.com>
2023-04-13 17:46:05 -0700

    Se reformateó el código

--- src/components/navigation/Navbar.svelte
+++ src/components/navigation/Navbar.svelte
@@ -8,7 +8,11 @@
 	class="fixed inset-y-0 left-0 w-64 p-4 pt-0 bg-stone-900 border-r border-stone-800 shadow-xl transition-all"
 >
 	<nav class="flex flex-col">
-		<a href="/home" data-sveltekit-preload-data class="p-4 outline-none focus:outline-pink-700 rounded-xl"><PantherGamesLogo size={2} /></a>
+		<a
+			href="/home"
+			data-sveltekit-preload-data
+			class="p-4 outline-none focus:outline-pink-700 rounded-xl"><PantherGamesLogo size={2} /></a
+		>
 		{#each sections as section}
 			<NavButton href={section.href} text={section.text} icon={section.icon} />
 		{/each}
