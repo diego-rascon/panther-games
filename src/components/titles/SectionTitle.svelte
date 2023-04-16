178034a93700f14d969456d53d99f913d979dbed
DiegoRascon <joserascon2002@gmail.com>
2023-04-15 23:45:27 -0700

    Se agregó una prop opcional que determina si el título estará centrado o alineado a la izquierda

--- src/components/titles/SectionTitle.svelte
+++ src/components/titles/SectionTitle.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
+	export let centered: boolean = false;
 	export let text: string;
 </script>
 
-<h1 class="text-4xl font-lexend font-bold">{text}</h1>
+<h1 class="text-4xl font-lexend font-bold {centered ? 'text-center' : 'text-left'}">{text}</h1>
