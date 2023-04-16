48300a033e17601796b905971160206cdacb90e8
DiegoRascon <joserascon2002@gmail.com>
2023-04-15 18:47:44 -0700

    Ahora se llama una función que regresa a los productos con las plataformas

--- src/routes/home/+page.server.ts
+++ src/routes/home/+page.server.ts
@@ -8,44 +8,20 @@ export const load = async () => {
 			.order('categoria_id', { ascending: true });
 		return data ?? [];
 	};
-    const fetchProducts = async () => {
+	const fetchProducts = async () => {
+		const { data } = await supabase.rpc('products_platforms');
+		return data ?? [];
+	};
+	const fetchPlatforms = async () => {
 		const { data } = await supabase
-			.from('producto')
+			.from('plataforma')
 			.select()
-			.order('producto_id', { ascending: true });
+			.order('plataforma_id', { ascending: true });
 		return data ?? [];
 	};
 	return {
-        categories: fetchCategories(),
-        products: fetchProducts()
-    };
-}
-
-/*
-const fetchCategories = async () => {
-		let { data, error } = await supabase
-			.from('categoria')
-			.select('*')
-			.order('categoria_id', { ascending: true });
-		if (error) {
-			console.log('Error', error);
-		} else {
-			categories = data;
-		}
-		loadingCategories = false;
+		categories: fetchCategories(),
+		products: fetchProducts(),
+		platforms: fetchPlatforms()
 	};
-
-	const fetchProducts = async () => {
-		let { data, error } = await supabase
-			.from('producto')
-			.select('*')
-			.order('producto_id', { ascending: true });
-		if (error) {
-			console.log('Error', error);
-		} else {
-			products = data;
-		}
-		loadingProducts = false;
-	};
-
-*/
+};
