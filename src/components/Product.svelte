4b2a8a3b61883d8a100aab95f7e7d07598689b63
DiegoRascon <joserascon2002@gmail.com>
2023-04-15 18:48:13 -0700

    Ahora se muestran los datos correctos de los productos

--- src/components/Product.svelte
+++ src/components/Product.svelte
@@ -1,19 +1,20 @@
 <script lang="ts">
-	export let text: string;
-	export let category: number;
-
-	const isGame: boolean = category === 1;
+	export let name: string;
+	export let stock: number;
+	export let price: number;
+	export let platform: string;
+	export let isGame: boolean;
 </script>
 
 <div class="flex flex-col p-4 text-left bg-stone-900 rounded-xl transition-all">
-	<p class="text-xl font-lexend font-bold select-none">{text}</p>
+	<p class="text-xl font-lexend font-bold select-none">{name}</p>
 	<div class="mt-auto">
 		<div class="py-4">
 			{#if isGame}
-				<p class="text-sm"><strong>Plataforma:</strong> gaystation</p>
+				<p class="text-sm"><strong>Plataforma:</strong> {platform}</p>
 			{/if}
-			<p class="text-sm"><strong>Cantidad:</strong> 69</p>
-			<p class="pt-4 text-xl font-bold">$420</p>
+			<p class="text-sm"><strong>Cantidad:</strong> {stock}</p>
+			<p class="pt-4 text-xl font-bold">$ {price}</p>
 		</div>
 		<button
 			class=" w-full py-2 hover:bg-stone-800 active:bg-stone-950 outline-none focus:outline-pink-700 border-2 border-pink-700 transition-all rounded-xl select-none"
