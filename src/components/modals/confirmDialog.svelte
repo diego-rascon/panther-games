dede83f05fda1fb803139e94b5b260202d2c21f7
DiegoRascon <joserascon2002@gmail.com>
2023-04-15 23:46:17 -0700

    Ventana simple para confirmación

---
+++ src/components/modals/confirmDialog.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import { fade, scale } from 'svelte/transition';
+	import SectionTitle from '../titles/SectionTitle.svelte';
+
+	type ClickHandler = () => void;
+
+	export let cancelHandler: ClickHandler;
+	export let confirmHandler: ClickHandler;
+	export let title: string;
+	export let text: string;
+
+	const animDuration = 150;
+</script>
+
+<div
+	class="fixed inset-0 flex justify-center items-center bg-black transition-all bg-opacity-75"
+	transition:fade={{ duration: animDuration }}
+>
+	<div
+		class="flex flex-col p-8 space-y-8 bg-stone-950 border border-stone-700 rounded-xl"
+		transition:scale={{ duration: animDuration }}
+	>
+		<SectionTitle centered={true} text={title} />
+		<p>{text}</p>
+		<div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
+			<button
+				class="py-2 border-2 border-pink-600 hover:bg-stone-900 active:bg-black outline-none focus:outline-pink-600 rounded-xl transition-all select-none"
+				on:click={cancelHandler}
+			>
+				Cancelar
+			</button>
+			<button
+				class="py-2 btn-fill font-bold rounded-xl transition-all select-none"
+				on:click={confirmHandler}
+			>
+				Confirmar
+			</button>
+		</div>
+	</div>
+</div>
