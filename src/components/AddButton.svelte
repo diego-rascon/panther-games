b7d80e987aca232658aecdc5ec26fc721ed24d76
DiegoRascon <joserascon2002@gmail.com>
2023-04-17 11:58:10 -0700

    Se eliminó la prop opcional del carrito de compras

--- src/components/AddButton.svelte
+++ src/components/AddButton.svelte
@@ -4,15 +4,12 @@
 
 	type ClickHandler = () => void;
 
-	export let cartVisible: boolean = false;
 	export let clickHandler: ClickHandler;
 </script>
 
 <button
 	on:click={clickHandler}
-	class="fixed bottom-0 flex m-4 p-4 items-center space-x-2 btn-fill rounded-xl shadow-xl transition-all {cartVisible
-		? 'right-64'
-		: 'right-0'}"
+	class="flex m-4 p-4 items-center space-x-2 btn-fill rounded-xl shadow-xl transition-all"
 >
 	<Icon icon={addCircleOutline} height={32} />
 	<p class="font-bold">Agregar</p>
