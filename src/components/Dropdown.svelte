b5552c4303f544b879898c8e23366f0dc3b8d09f
DiegoRascon <joserascon2002@gmail.com>
2023-04-16 21:16:33 -0700

    Componente de dropdown

---
+++ src/components/Dropdown.svelte
@@ -0,0 +1,19 @@
+<div
+	class="dropdown-content bg-stone-950 border border-stone-700 rounded-md text-center w-25 transition-all"
+	style="position: absolute; left: 70%; top: 0; margin-top: 2.8rem;"
+>
+	<ul class="w-auto">
+		<li><button class="w-full py-2 hover:bg-stone-800 active:bg-stone-950">Editar</button></li>
+		<li>
+			<button class="w-full py-2 hover:bg-stone-800 active:bg-stone-950">Cambio Stock</button>
+		</li>
+		<li><button class="w-full py-2 hover:bg-stone-800 active:bg-stone-950">Eliminar</button></li>
+	</ul>
+</div>
+
+<style>
+	.dropdown-content {
+		position: absolute;
+		z-index: 9999;
+	}
+</style>
