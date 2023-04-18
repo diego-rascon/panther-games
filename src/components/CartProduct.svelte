6b850ecabfeceb26aa05eef6e0f370d61d4a0536
DiegoRascon <joserascon2002@gmail.com>
2023-04-18 09:23:49 -0700

    Ahora se llama el método de eliminar del carrito

--- src/components/CartProduct.svelte
+++ src/components/CartProduct.svelte
@@ -1,7 +1,36 @@
 <script lang="ts">
-    export let name: string;
+	import Icon from '@iconify/svelte';
+	import trashBinMinimalisticOutline from '@iconify/icons-solar/trash-bin-minimalistic-outline';
+
+	type ClickHandler = (carritoId: number) => void;
+
+	const removeFromCart = () => {
+		removeHandler(id);
+	};
+
+	export let removeHandler: ClickHandler;
+	export let id: number;
+	export let name: string;
+	export let price: number;
+	export let stock: number;
 </script>
 
-<div>
-    <p>{name}</p>
+<div class="p-4 bg-stone-800 rounded-xl space-y-2">
+	<div class="flex justify-between">
+		<p>{name}</p>
+		<p>$ {price}</p>
+	</div>
+	<div class="flex justify-between">
+		<select class="p-1 bg-stone-900 rounded-xl select-none">
+			{#each Array(stock) as _, index (index)}
+				<option>{index + 1}</option>
+			{/each}
+		</select>
+		<button
+			on:click={removeFromCart}
+			class="p-1 bg-stone-900 hover:bg-stone-700 active:bg-stone-950 rounded-xl"
+		>
+			<Icon icon={trashBinMinimalisticOutline} height={20} />
+		</button>
+	</div>
 </div>
