690d0456929871bedfcc0d8321ff57b123eb3242
DiegoRascon <joserascon2002@gmail.com>
2023-04-17 21:14:01 -0700

    Ahora se llama la functión de clients_members() para obtener a los miembros

--- src/routes/home/members/+page.server.ts
+++ src/routes/home/members/+page.server.ts
@@ -2,21 +2,8 @@ import { supabase } from '$lib/db';
 
 export const load = async () => {
 	const fetchMembers = async () => {
-		const { data, error } = await supabase
-			.from('cliente')
-			.select()
-			.eq('cliente_miembro', true)
-			.order('cliente_id', { ascending: false });
-		if (error) console.log(error);
-		if (data) console.log(data);
-
+		const { data } = await supabase.rpc('clients_members');
 		return data ?? [];
 	};
-	/*
-	const fetchMembers = async () => {
-		const { data } = await supabase.rpc('members');
-		return data ?? [];
-	};
-	*/
 	return { members: fetchMembers() };
 };
