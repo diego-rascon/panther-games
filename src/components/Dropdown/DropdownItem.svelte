820a81e4881cf91e99a16bc53c07ce0f30aa4c79
DiegoRascon <joserascon2002@gmail.com>
2023-04-18 19:41:24 -0700

    Componente para cada opción del menú de dropdown

---
+++ src/components/Dropdown/DropdownItem.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+	import type { IconifyIcon } from '@iconify/types';
+
+	export let text: string;
+	export let icon: IconifyIcon;
+</script>
+
+<button class="flex items-center px-4 py-2 space-x-4 hover:bg-stone-800 active:bg-stone-950 rounded-xl transition-all">
+	<Icon {icon} height={20} />
+	<p>{text}</p>
+</button>
