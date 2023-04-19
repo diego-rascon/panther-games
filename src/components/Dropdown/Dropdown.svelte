a3d0874760b695e68623d50fd1f84a7d33678f63
DiegoRascon <joserascon2002@gmail.com>
2023-04-18 19:40:23 -0700

    Se movieron los componentes del dropdown en su propia carpeta

---
+++ src/components/Dropdown/Dropdown.svelte
@@ -0,0 +1,16 @@
+<script>
+	import DropdownItem from './DropdownItem.svelte';
+	import pen2Outline from '@iconify/icons-solar/pen-2-outline';
+	import trashBinMinimalisticOutline from '@iconify/icons-solar/trash-bin-minimalistic-outline';
+	import boxOutline from '@iconify/icons-solar/box-outline';
+	import { scale } from 'svelte/transition';
+</script>
+
+<div
+	class="absolute top-10 right-4 flex flex-col p-2 bg-stone-900 border border-stone-700 rounded-xl shadow-xl origin-top-right"
+	transition:scale={{ duration: 150 }}
+>
+	<DropdownItem text="Editar" icon={pen2Outline} />
+	<DropdownItem text="Eliminar" icon={trashBinMinimalisticOutline} />
+	<DropdownItem text="Modificar stock" icon={boxOutline} />
+</div>
