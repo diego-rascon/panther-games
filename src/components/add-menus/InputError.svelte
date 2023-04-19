d2a2089a865a9f5742be25c8a4162d3ca8f9fc56
DiegoRascon <joserascon2002@gmail.com>
2023-04-18 22:46:08 -0700

    Se agregó la prop isWarning

--- src/components/add-menus/InputError.svelte
+++ src/components/add-menus/InputError.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
+	export let isWarning: boolean = false;
 	export let text: string;
 </script>
 
-<p class="text-rose-400">{text}</p>
+<p class={isWarning ? 'text-amber-600' : 'text-red-600'}>{text}</p>
