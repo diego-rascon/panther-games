c22f1076aeaa13ac23926abe33644368e446231f
DiegoRascon <joserascon2002@gmail.com>
2023-04-20 00:16:04 -0700

    Se cambió el tipo del ícono a Linear

--- src/components/utils/ErrorScreen.svelte
+++ src/components/utils/ErrorScreen.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	import Icon from '@iconify/svelte';
-	import sadSquareOutline from '@iconify/icons-solar/sad-square-outline';
+	import sadSquareLinear from '@iconify/icons-solar/sad-square-linear';
 
 	export let text: string;
 </script>
 
 <div class="flex flex-col items-center justify-center p-4 space-y-4 transition-all">
-	<Icon icon={sadSquareOutline} height={64} />
+	<Icon icon={sadSquareLinear} height={64} />
 	<p>Ocurrió un error cargando los datos ({text}).</p>
 </div>
