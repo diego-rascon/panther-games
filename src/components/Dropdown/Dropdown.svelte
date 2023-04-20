608ef375b865381af892ff7db07b55dba8f4e881
DiegoRascon <joserascon2002@gmail.com>
2023-04-20 01:17:50 -0700

    Ahora se hace espacio para que se muestren los items del dropdown

--- src/components/Dropdown/Dropdown.svelte
+++ src/components/Dropdown/Dropdown.svelte
@@ -1,16 +1,10 @@
-<script>
-	import DropdownItem from './DropdownItem.svelte';
-	import pen2Linear from '@iconify/icons-solar/pen-2-linear';
-	import trashBinMinimalisticLinear from '@iconify/icons-solar/trash-bin-minimalistic-linear';
-	import boxLinear from '@iconify/icons-solar/box-linear';
+<script lang="ts">
 	import { scale } from 'svelte/transition';
 </script>
 
 <div
-	class="absolute top-10 right-4 flex flex-col p-2 bg-stone-900 border border-stone-700 rounded-xl shadow-xl origin-top-right"
+	class="absolute top-12 right-4 flex flex-col p-2 bg-stone-900 border border-stone-700 rounded-xl shadow-xl origin-top-right"
 	transition:scale={{ duration: 150 }}
 >
-	<DropdownItem text="Editar" icon={pen2Linear} />
-	<DropdownItem text="Eliminar" icon={trashBinMinimalisticLinear} />
-	<DropdownItem text="Modificar stock" icon={boxLinear} />
+	<slot />
 </div>
