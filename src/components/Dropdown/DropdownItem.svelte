fa2f6087b2dfa3bc01ed357150579351def3975e
DiegoRascon <joserascon2002@gmail.com>
2023-04-20 01:18:15 -0700

    Se eliminó el funcionamiento del botón

--- src/components/Dropdown/DropdownItem.svelte
+++ src/components/Dropdown/DropdownItem.svelte
@@ -2,11 +2,15 @@
 	import Icon from '@iconify/svelte';
 	import type { IconifyIcon } from '@iconify/types';
 
+	type ClickHandler = () => void;
+
 	export let text: string;
 	export let icon: IconifyIcon;
 </script>
 
-<button class="flex items-center px-4 py-2 space-x-4 hover:bg-stone-800 active:bg-stone-950 rounded-xl transition-all">
+<button
+	class="flex items-center px-4 py-2 space-x-4 hover:bg-stone-800 active:bg-stone-950 rounded-xl transition-all"
+>
 	<Icon {icon} height={20} />
 	<p>{text}</p>
 </button>
