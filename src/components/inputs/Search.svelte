0b6a966fe03302e24340150a455e4575b91828c7
DiegoRascon <joserascon2002@gmail.com>
2023-04-20 00:10:18 -0700

    Componente de la barra de busqueda

--- src/components/inputs/Search.svelte
+++ src/components/inputs/Search.svelte
@@ -7,20 +7,34 @@
 	export let searchClient: SearchHandler;
 	export let search: string;
 
+	let searching: boolean = false;
+
 	const searchHandler = () => {
 		searchClient(search);
+	};
+
+	const searchFocusGain = () => {
+		searching = true;
+	};
+
+	const searchFocusLoss = () => {
+		searching = false;
 	};
 </script>
 
 <div
-	class="flex items-center px-4 w-full max-w-md bg-stone-900 rounded-xl outline-none active:outline-pink-700 transition-all"
+	class="flex items-center px-4 space-x-4 w-full rounded-xl outline-none active:outline-pink-700 shadow-2xl transition-all {searching
+		? 'max-w-lg bg-stone-900 outline-pink-700'
+		: 'max-w-xs bg-stone-800'}"
 >
+	<Icon icon={magniferLinear} height={20} />
 	<input
 		type="text"
 		bind:value={search}
 		on:input={searchHandler}
+		on:focus={searchFocusGain}
+		on:blur={searchFocusLoss}
 		class="w-full bg-transparent outline-none"
 		placeholder="Buscar..."
 	/>
-	<Icon icon={magniferLinear} height={20} />
 </div>
