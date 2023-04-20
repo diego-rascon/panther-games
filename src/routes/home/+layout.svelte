c5c20b0429c4e662a4bba3e8e72ec067993553af
DiegoRascon <joserascon2002@gmail.com>
2023-04-19 20:24:22 -0700

    Se eliminó el componente Header.svelte

--- src/routes/home/+layout.svelte
+++ src/routes/home/+layout.svelte
@@ -1,12 +1,10 @@
 <script>
-	import Header from '../../components/navigation/Header.svelte';
 	import Navbar from '../../components/navigation/Navbar.svelte';
 </script>
 
 <div class="min-h-screen bg-stone-950">
 	<Navbar />
 	<div class="flex-1 p-4 ml-64">
-		<Header />
 		<slot />
 	</div>
 </div>
