59f84c3e6c3f524467a354d99122850de9727f66
DiegoRascon <joserascon2002@gmail.com>
2023-04-19 23:48:47 -0700

    Se cambió el tipo del ícono a outline

--- src/components/AddButton.svelte
+++ src/components/AddButton.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import Icon from '@iconify/svelte';
-	import addCircleOutline from '@iconify/icons-solar/add-circle-outline';
+	import addCircleLinear from '@iconify/icons-solar/add-circle-linear';
 
 	type ClickHandler = () => void;
 
@@ -11,6 +11,6 @@
 	on:click={clickHandler}
 	class="flex m-4 p-4 items-center space-x-2 btn-fill rounded-xl shadow-xl transition-all"
 >
-	<Icon icon={addCircleOutline} height={32} />
+	<Icon icon={addCircleLinear} height={32} />
 	<p class="font-bold">Agregar</p>
 </button>
