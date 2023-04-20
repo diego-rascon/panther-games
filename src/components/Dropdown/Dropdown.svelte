1c0d533dcdeccff62387a4a2e3a6a1f162e0a922
DiegoRascon <joserascon2002@gmail.com>
2023-04-19 23:49:25 -0700

    Se cambiaron los tipos de los íconos a outline

--- src/components/Dropdown/Dropdown.svelte
+++ src/components/Dropdown/Dropdown.svelte
@@ -1,8 +1,8 @@
 <script>
 	import DropdownItem from './DropdownItem.svelte';
-	import pen2Outline from '@iconify/icons-solar/pen-2-outline';
-	import trashBinMinimalisticOutline from '@iconify/icons-solar/trash-bin-minimalistic-outline';
-	import boxOutline from '@iconify/icons-solar/box-outline';
+	import pen2Linear from '@iconify/icons-solar/pen-2-linear';
+	import trashBinMinimalisticLinear from '@iconify/icons-solar/trash-bin-minimalistic-linear';
+	import boxLinear from '@iconify/icons-solar/box-linear';
 	import { scale } from 'svelte/transition';
 </script>
 
@@ -10,7 +10,7 @@
 	class="absolute top-10 right-4 flex flex-col p-2 bg-stone-900 border border-stone-700 rounded-xl shadow-xl origin-top-right"
 	transition:scale={{ duration: 150 }}
 >
-	<DropdownItem text="Editar" icon={pen2Outline} />
-	<DropdownItem text="Eliminar" icon={trashBinMinimalisticOutline} />
-	<DropdownItem text="Modificar stock" icon={boxOutline} />
+	<DropdownItem text="Editar" icon={pen2Linear} />
+	<DropdownItem text="Eliminar" icon={trashBinMinimalisticLinear} />
+	<DropdownItem text="Modificar stock" icon={boxLinear} />
 </div>
