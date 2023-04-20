f30f9bb69493ee729c1ba1d21a738755fbf824a8
DiegoRascon <joserascon2002@gmail.com>
2023-04-20 00:14:52 -0700

    Se puso el componente Icon en un div que gira

--- src/components/utils/LoadingScreen.svelte
+++ src/components/utils/LoadingScreen.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import Icon from '@iconify/svelte';
-	import refreshCircleOutline from '@iconify/icons-solar/refresh-circle-outline';
+	import refreshCircleLinear from '@iconify/icons-solar/refresh-circle-linear';
 
 	export let text: string;
 </script>
 
 <div class="flex items-center justify-center p-4 space-x-2 transition-all">
-	<Icon class="animate-spin" icon={refreshCircleOutline} height={28} hFlip={true} />
+	<div class="animate-spin">
+		<Icon icon={refreshCircleLinear} height={28} hFlip={true} />
+	</div>
 	<p>Cargando {text}...</p>
 </div>
