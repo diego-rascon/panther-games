53d2308a6c177744638f2e9758bc9840de0dc64f
DiegoRascon <joserascon2002@gmail.com>
2023-04-20 20:09:05 -0700

    Se eliminó la prop de la función

--- src/components/AddButton.svelte
+++ src/components/AddButton.svelte
@@ -1,16 +1,9 @@
 <script lang="ts">
 	import Icon from '@iconify/svelte';
 	import addCircleLinear from '@iconify/icons-solar/add-circle-linear';
-
-	type ClickHandler = () => void;
-
-	export let clickHandler: ClickHandler;
 </script>
 
-<button
-	on:click={clickHandler}
-	class="flex m-4 p-4 items-center space-x-2 btn-fill rounded-xl shadow-xl transition-all"
->
+<button on:click class="flex m-4 p-4 items-center space-x-2 btn-fill rounded-xl shadow-xl transition-all">
 	<Icon icon={addCircleLinear} height={32} />
 	<p class="font-bold">Agregar</p>
 </button>
