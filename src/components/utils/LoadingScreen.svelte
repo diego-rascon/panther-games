0104a6b4479ee9704ec42e7ff31ae26a45c2d700
DiegoRascon <joserascon2002@gmail.com>
2023-04-21 00:43:46 -0700

    Se eliminó la prop de texto

--- src/components/utils/LoadingScreen.svelte
+++ src/components/utils/LoadingScreen.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
 	import Icon from '@iconify/svelte';
 	import refreshCircleOutline from '@iconify/icons-solar/refresh-circle-outline';
-
-	export let text: string;
 </script>
-
 <div class="flex items-center justify-center p-4 space-x-2 transition-all">
-	<Icon class="animate-spin" icon={refreshCircleOutline} height={28} hFlip={true} />
-	<p>Cargando {text}...</p>
+	<div class="animate-spin">
+		<Icon icon={refreshCircleOutline} height={28} hFlip={true} />
+	</div>
+	<p>Cargando...</p>
 </div>
