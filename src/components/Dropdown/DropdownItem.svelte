a4b79e173be4b6dee16b9222477d2d129b4ec289
DiegoRascon <joserascon2002@gmail.com>
2023-04-20 20:32:09 -0700

    Se eliminó el tipo ClickHandler

--- src/components/Dropdown/DropdownItem.svelte
+++ src/components/Dropdown/DropdownItem.svelte
@@ -2,13 +2,12 @@
 	import Icon from '@iconify/svelte';
 	import type { IconifyIcon } from '@iconify/types';
 
-	type ClickHandler = () => void;
-
 	export let text: string;
 	export let icon: IconifyIcon;
 </script>
 
 <button
+	on:click
 	class="flex items-center px-4 py-2 space-x-4 hover:bg-stone-800 active:bg-stone-950 rounded-xl transition-all"
 >
 	<Icon {icon} height={20} />
