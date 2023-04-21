5951db9dbd729208297902684793ee1d7fc2cb83
DiegoRascon <joserascon2002@gmail.com>
2023-04-21 15:59:58 -0700

    Componente para cada venta

---
+++ src/components/SaleRow.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+	import menuDotsBold from '@iconify/icons-solar/menu-dots-bold';
+	import Dropdown from './Dropdown/Dropdown.svelte';
+	import DropdownItem from './Dropdown/DropdownItem.svelte';
+	import pen2Linear from '@iconify/icons-solar/pen-2-linear';
+	import boxLinear from '@iconify/icons-solar/box-linear';
+	import trashBinMinimalisticLinear from '@iconify/icons-solar/trash-bin-minimalistic-linear';
+
+	export let id: number;
+	export let total: number;
+	export let discount: number;
+	export let date: string;
+
+	let dropdownVisible = false;
+
+	const toggleDropdown = () => {
+		dropdownVisible = !dropdownVisible;
+	};
+</script>
+
+<tr>
+	<td class="p-2 text-left">{id}</td>
+	<td class="p-2 text-left">{total}</td>
+	<td class="p-2 text-left">{discount}</td>
+	<td class="p-2 text-left">{date}</td>
+	<td class="py-2 text-right">
+		<button
+			on:click={toggleDropdown}
+			class="rounded-full p-2 hover:bg-stone-800 active:bg-stone-950 transition-all"
+		>
+			<Icon icon={menuDotsBold} rotate={1} height={24} />
+		</button>
+	</td>
+</tr>
