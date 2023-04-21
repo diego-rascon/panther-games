4a5f0e3c4bab3161794e28419011f1506a7994dd
fungirox <annxrk13@gmail.com>
2023-04-20 17:51:57 -0700

    En el reporte general se implementaron datos reales en tabla consolas y ventas

--- src/routes/home/reports/+page.server.ts
+++ src/routes/home/reports/+page.server.ts
@@ -1,6 +1,21 @@
 import { supabase } from '$lib/db';
 
 export const load = async () => {
-	const { data } = await supabase.rpc('generalreportgames');
-	return { games: data ?? [],};
+	const fetchGames = async () => {
+		const { data } = await supabase.rpc('generalreportgamescal');
+		return data ?? [];
+	};
+	const fetchConsolas = async () => {
+		const { data } = await supabase.rpc('generalreportconsolas');
+		return data ?? [];
+	};
+	const fetchVentas = async () => {
+		const { data } = await supabase.rpc('generalreportventas');
+		return data ?? [];
+	};
+	return {
+		games: fetchGames(),
+		consolas: fetchConsolas(),
+		ventas: fetchVentas()
+	};
 };
