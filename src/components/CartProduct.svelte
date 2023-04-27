120874e904b5ae2f0a0af188c8ba7a52cc9abb94
DiegoRascon <joserascon2002@gmail.com>
2023-04-26 20:29:43 -0700

    Se aumentó el tamaño de los botónes y se limitó el texto del título a dos lineas

--- src/components/CartProduct.svelte
+++ src/components/CartProduct.svelte
@@ -17,7 +17,7 @@
 
 <div class="p-4 bg-stone-800 rounded-xl space-y-2">
 	<div class="flex justify-between">
-		<p class="line-clamp-4 w-32">{name}</p>
+		<p class="line-clamp-2 w-32">{name}</p>
 		<p>$ {quantity * price}</p>
 	</div>
 	<div class="flex justify-end space-x-2">
@@ -29,13 +29,13 @@
 			on:change={() => {
 				quantityHandler(cartId, quantity);
 			}}
-			class="w-12 px-2 py-1 bg-stone-900 outline-pink rounded-xl select-none"
+			class="w-12 p-2 bg-stone-900 outline-pink rounded-xl select-none"
 		/>
 		<button
 			on:click={() => {
 				removeHandler(cartId, productId);
 			}}
-			class="px-2 py-1 bg-stone-900 hover:bg-stone-700 active:bg-stone-950 outline-pink rounded-xl"
+			class="p-2 bg-stone-900 hover:bg-stone-700 active:bg-stone-950 outline-pink rounded-xl"
 		>
 			<Icon icon={trashBinMinimalisticLinear} height={20} />
 		</button>
