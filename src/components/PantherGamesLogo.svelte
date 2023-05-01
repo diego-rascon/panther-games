072db57d2b5c704c9698fdca1018004aa6f1e994
DiegoRascon <joserascon2002@gmail.com>
2023-05-01 12:29:41 -0700

    Se movió el archivo a la carpeta de títulos

--- src/components/PantherGamesLogo.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	export let size: Number;
-</script>
-
-<div class="flex flex-col text-center -space-y-2 text-pink-700 hover:text-pink-600 active:text-pink-800 select-none transition-all">
-	<h2 class="font-bold">panther</h2>
-	<h2>games</h2>
-</div>
---
+++ src/components/titles/PantherGamesLogo.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let size: Number;
+</script>
+
+<div class="unstyled flex flex-col text-center -space-y-2 text-{size}xl font-lexend text-pink-700 hover:text-pink-600 active:text-pink-800 select-none transition-all">
+	<span class="font-bold">panther</span>
+	<span>games</span>
+</div>
