210a4d4ee8b6e77ad08907daa1d9bd4af1fee747
DiegoRascon <joserascon2002@gmail.com>
2023-05-01 00:41:01 -0700

    Ahora se utiliza h2

--- src/components/PantherGamesLogo.svelte
+++ src/components/PantherGamesLogo.svelte
@@ -2,7 +2,7 @@
 	export let size: Number;
 </script>
 
-<div class="flex flex-col text-center -space-y-2 text-{size}xl font-lexend text-pink-700 hover:text-pink-600 active:text-pink-800 select-none transition-all">
-	<h1 class="font-bold">panther</h1>
-	<h1>games</h1>
+<div class="flex flex-col text-center -space-y-2 text-pink-700 hover:text-pink-600 active:text-pink-800 select-none transition-all">
+	<h2 class="font-bold">panther</h2>
+	<h2>games</h2>
 </div>
