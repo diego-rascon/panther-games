1b89cde33fe6386ed479061ac3ea4a228d37fc17
DiegoRascon <joserascon2002@gmail.com>
2023-05-01 16:59:28 -0700

    Componente para usar modales

---
+++ src/components/modals/DarkenSreen.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { fade } from 'svelte/transition';
+</script>
+
+<div
+	class="fixed inset-0 flex justify-center items-center bg-black transition-all bg-opacity-75"
+	transition:fade={{ duration: 150 }}
+>
+	<slot />
+</div>
