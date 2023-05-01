2166c7ace95e21fe4580f7b671f94438a51171ac
DiegoRascon <joserascon2002@gmail.com>
2023-05-01 00:40:24 -0700

    Ahora utiliza las variantes de skeleton

--- src/components/AddButton.svelte
+++ src/components/AddButton.svelte
@@ -3,7 +3,7 @@
 	import addCircleLinear from '@iconify/icons-solar/add-circle-linear';
 </script>
 
-<button on:click class="flex m-4 p-4 items-center space-x-2 btn-fill rounded-xl shadow-xl transition-all">
+<button on:click class="flex m-4 p-4 space-x-2 btn variant-filled-primary shadow-xl">
 	<Icon icon={addCircleLinear} height={32} />
 	<p class="font-bold">Agregar</p>
 </button>
