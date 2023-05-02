8c82126301bcc7ea2969bad5243da961b949609d
DiegoRascon <joserascon2002@gmail.com>
2023-05-01 19:57:57 -0700

    Se implementó el popup de Skeleton

--- src/components/Product.svelte
+++ src/components/Product.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
+	import { activeProducts } from '$lib/stores';
 	import { popup } from '@skeletonlabs/skeleton';
 	import type { PopupSettings } from '@skeletonlabs/skeleton';
+	import Dropdown from './dropdown/Dropdown.svelte';
+	import DropdownItem from './dropdown/DropdownItem.svelte';
 	import Icon from '@iconify/svelte';
 	import menuDotsBold from '@iconify/icons-solar/menu-dots-bold';
 	import pen2Linear from '@iconify/icons-solar/pen-2-linear';
 	import boxLinear from '@iconify/icons-solar/box-linear';
 	import trashBinMinimalisticLinear from '@iconify/icons-solar/trash-bin-minimalistic-linear';
-	import { activeProducts } from '$lib/stores';
 
 	export let addToCart: (productId: number) => void;
 	export let editProduct: (productId: number) => void;
@@ -21,28 +23,20 @@
 
 	$: onCart = $activeProducts.some((item: any) => item.productId == id);
 
-	let dropdownVisible = false;
-
-	const toggleDropdown = () => {
-		dropdownVisible = !dropdownVisible;
-	};
-
-	const closeDropdown = () => {
-		dropdownVisible = false;
+	let dropdown: PopupSettings = {
+		placement: 'bottom-end',
+		event: 'focus-click',
+		target: 'dropdown'
 	};
 </script>
 
-<div class="relative flex flex-col p-4 text-left bg-stone-900 rounded-xl transition-all">
+<div class="flex flex-col p-4 text-left bg-stone-900 rounded-xl transition-all">
 	<div class="flex items-start justify-between">
 		<p class="unstyled line-clamp-4 text-xl font-bold select-none">{name}</p>
-		<button
-			class="ml-2 rounded-full p-1 hover:bg-stone-800 active:bg-stone-950 transition-all {dropdownVisible
-				? 'bg-stone-800'
-				: ''}"
-		>
+		<button on:click|stopPropagation  use:popup={dropdown} class="ml-2 btn-icon hover:variant-soft">
 			<Icon icon={menuDotsBold} rotate={1} height={18} />
 		</button>
-		<!--
+		<div data-popup="dropdown">
 			<Dropdown>
 				<DropdownItem
 					text="Editar"
@@ -66,6 +60,9 @@
 					}}
 				/>
 			</Dropdown>
+		</div>
+		<!--
+			
 			-->
 	</div>
 	<div class="flex flex-col mt-auto">
