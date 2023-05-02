e2e3608160fc9a08b3fedaf0f31f22e6c4d5f824
DiegoRascon <joserascon2002@gmail.com>
2023-05-01 20:46:56 -0700

    Ahora el espaciado entre los elementos es de 4

--- src/components/modals/confirmDialog.svelte
+++ src/components/modals/confirmDialog.svelte
@@ -9,22 +9,14 @@
 </script>
 
 <div
-	class="flex flex-col p-8 space-y-8 bg-stone-950 border border-stone-800 rounded-xl"
+	class="flex flex-col p-8 space-y-4 bg-stone-950 border border-stone-800 rounded-xl"
 	in:scale={{ duration: 150 }}
 >
 	<SectionTitle centered={true} text={title} />
 	<p>{text}</p>
-	<div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
-		<button
-			class="btn variant-ringed-primary"
-			on:click={cancelHandler}
-		>
-			Cancelar
-		</button>
-		<button
-			class="btn variant-filled-primary font-bold"
-			on:click={confirmHandler}
-		>
+	<div class="grid grid-cols-2 gap-4">
+		<button class="btn variant-ringed-primary" on:click={cancelHandler}> Cancelar </button>
+		<button class="btn variant-filled-primary font-bold" on:click={confirmHandler}>
 			Confirmar
 		</button>
 	</div>
