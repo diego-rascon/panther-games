f8faf080015c00c24edfba7af211402eb432629b
DiegoRascon <joserascon2002@gmail.com>
2023-05-01 18:58:14 -0700

    Se cambió el color del borde

--- src/components/Dropdown/Dropdown.svelte
+++ src/components/Dropdown/Dropdown.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-	class="absolute z-50 top-12 right-4 flex flex-col p-2 bg-stone-900 border border-stone-700 rounded-xl shadow-xl origin-top-right"
+	class="absolute z-50 top-12 right-4 flex flex-col p-2 bg-stone-900 border border-stone-800 rounded-xl shadow-xl origin-top-right"
 	transition:scale={{ duration: 150 }}
 >
 	<slot />
