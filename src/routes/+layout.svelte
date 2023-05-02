ef842998854c2e408b966c1b7c422b9a51f23147
DiegoRascon <joserascon2002@gmail.com>
2023-05-01 19:26:39 -0700

    Se importan los objetos necesarios para utilizar popups

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
 	import '../theme.postcss';
 	import '@skeletonlabs/skeleton/styles/all.css';
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
 	import { Toast } from '@skeletonlabs/skeleton';
+
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
 
 <Toast />
