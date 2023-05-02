98a9e4bbd200640e41584cbcec912aab49bd52e3
DiegoRascon <joserascon2002@gmail.com>
2023-05-01 20:45:54 -0700

    Se eliminó la clase origin-top-right

--- src/components/Dropdown/Dropdown.svelte
+++ src/components/Dropdown/Dropdown.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-	class="z-[999] flex flex-col p-2 bg-stone-900 border border-stone-800 rounded-xl shadow-xl origin-top-right"
+	class="z-[999] flex flex-col p-2 bg-stone-900 border border-stone-800 rounded-xl shadow-xl"
 	transition:scale={{ duration: 150 }}
 >
 	<slot />
