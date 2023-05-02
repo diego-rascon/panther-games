4982891e0a0e5176885de3984d5863ff5edde3a4
DiegoRascon <joserascon2002@gmail.com>
2023-05-01 18:20:41 -0700

    Se cambió el espaciado de los elementos principales de 8 a 4

--- src/components/forms/ProductForm.svelte
+++ src/components/forms/ProductForm.svelte
@@ -61,35 +61,33 @@
 	/>
 {:else}
 	<div
-		class="flex flex-col p-8 space-y-8 bg-stone-950 border border-stone-700 rounded-xl transition-all"
+		class="flex flex-col p-8 space-y-4 bg-stone-950 border border-stone-700 rounded-xl transition-all"
 		in:scale={{ duration: 150 }}
 	>
 		<SectionTitle text="Registrar Producto" />
-		<div class="flex flex-col space-y-4 transition-all">
-			<select class="input" bind:value={categoryId}>
-				{#each categories as category}
-					<option value={category.categoria_id}>{category.categoria_nombre}</option>
+		<select class="input" bind:value={categoryId}>
+			{#each categories as category}
+				<option value={category.categoria_id}>{category.categoria_nombre}</option>
+			{/each}
+		</select>
+		{#if categoryId === 1}
+			<select class="input" bind:value={platformId}>
+				{#each platforms as platform}
+					<option value={platform.plataforma_id}>{platform.plataforma_nombre}</option>
 				{/each}
 			</select>
-			{#if categoryId === 1}
-				<select class="input" bind:value={platformId}>
-					{#each platforms as platform}
-						<option value={platform.plataforma_id}>{platform.plataforma_nombre}</option>
-					{/each}
-				</select>
-			{/if}
-			<input type="text" bind:value={name} class="input" placeholder="Nombre" />
-			<input type="number" bind:value={price} class="input" placeholder="Precio" />
-			<input type="number" bind:value={stock} class="input" placeholder="Stock" />
-			<input type="number" bind:value={minimumStock} class="input" placeholder="Mínimo de stock" />
-			<SlideToggle bind:checked={used} name="slider-sm" active="bg-primary-500" size="sm">
-				Producto usado
-			</SlideToggle>
-			{#if inputError}
-				<InputError text={errorMessage} />
-			{/if}
-		</div>
-		<div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
+		{/if}
+		<input type="text" bind:value={name} class="input" placeholder="Nombre" />
+		<input type="number" bind:value={price} class="input" placeholder="Precio" />
+		<input type="number" bind:value={stock} class="input" placeholder="Stock" />
+		<input type="number" bind:value={minimumStock} class="input" placeholder="Mínimo de stock" />
+		<SlideToggle bind:checked={used} name="slider-sm" active="bg-primary-500" size="sm">
+			Producto usado
+		</SlideToggle>
+		{#if inputError}
+			<InputError text={errorMessage} />
+		{/if}
+		<div class="grid grid-cols-2 gap-4">
 			<button class="btn variant-ringed-primary" on:click={cancelHandler}> Cancelar </button>
 			<button class="btn variant-filled-primary font-bold" on:click={toggleConfirmation}>
 				Confirmar
