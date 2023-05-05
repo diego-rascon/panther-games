db0c19e1ec7f1f3dda7f947b4c8b3bbf5808f395
DiegoRascon <joserascon2002@gmail.com>
2023-05-05 12:43:50 -0700

    Componente que se muestra cuando no se encuentra ningún resultado en una busqueda

---
+++ src/components/utils/NoResultsMessage.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Icon from '@iconify/svelte';
+</script>
+
+<div class="flex flex-col p-24 items-center space-y-4 text-stone-500">
+	<Icon icon="solar:sad-square-outline" height={64} color="" />
+	<span>No se encontraron resultados para la busqueda.</span>
+</div>
