d8098c7076110bcecc65664993152686f98f7e3e
DiegoRascon <joserascon2002@gmail.com>
2023-05-05 15:25:08 -0700

    Se cambió el ícono de la sección de clientes

--- src/lib/sections.ts
+++ src/lib/sections.ts
@@ -2,7 +2,7 @@ import type { IconifyIcon } from '@iconify/types';
 import gamepadLinear from '@iconify/icons-solar/gamepad-linear';
 import dollarMinimalisticLinear from '@iconify/icons-solar/dollar-minimalistic-linear';
 import crownStarLinear from '@iconify/icons-solar/crown-star-linear';
-import userLinear from '@iconify/icons-solar/user-linear';
+import usersGroupRoundedOutline from '@iconify/icons-solar/users-group-rounded-outline';
 import documentTextLinear from '@iconify/icons-solar/document-text-linear';
 
 export type SectionData = {
@@ -15,6 +15,6 @@ export const sections: SectionData[] = [
 	{ href: '/home', text: 'Productos', icon: gamepadLinear },
 	{ href: '/home/sales', text: 'Ventas', icon: dollarMinimalisticLinear },
 	{ href: '/home/members', text: 'Miembros', icon: crownStarLinear },
-	{ href: '/home/clients', text: 'Clientes', icon: userLinear },
+	{ href: '/home/clients', text: 'Clientes', icon: usersGroupRoundedOutline },
 	{ href: '/home/reports', text: 'Reportes', icon: documentTextLinear }
 ];
