cc5106e9db4af41e083ee9717eb732c35f5b4fec
DiegoRascon <joserascon2002@gmail.com>
2023-05-05 15:26:08 -0700

    Se agregó una prop que determina si se está agregando o editando un cliente

--- src/components/forms/ClientForm.svelte
+++ src/components/forms/ClientForm.svelte
@@ -3,7 +3,11 @@
 	import SectionTitle from '../titles/SectionTitle.svelte';
 	import ConfirmDialog from '../modals/ConfirmDialog.svelte';
 	import InputError from '../utils/InputError.svelte';
-
+	import Icon from '@iconify/svelte';
+	import userOutline from '@iconify/icons-solar/user-outline';
+	import letterOutline from '@iconify/icons-solar/letter-outline';
+	import phoneCallingRoundedOutline from '@iconify/icons-solar/phone-calling-rounded-outline';
+	export let editing = false;
 	export let cancelHandler: () => void;
 	export let confirmHandler: () => void;
 	export let name: string;
@@ -52,26 +56,39 @@
 	<ConfirmDialog
 		cancelHandler={toggleConfirmation}
 		confirmHandler={addProduct}
-		title="Confirmar Registro"
-		text="¿Está seguro de que desea registrar al nuevo cliente?"
+		title={editing ? 'Confirmar Edición' : 'Confirmar Registro'}
+		text={editing
+			? '¿Está seguro de que desea editar al cliente?'
+			: '¿Está seguro de que desea registrar al nuevo cliente?'}
 	/>
 {:else}
 	<div
-		class="flex flex-col p-8 space-y-4 bg-stone-950 border border-stone-800 rounded-xl transition-all"
+		class="flex flex-col p-8 w-full max-w-md space-y-8 bg-stone-950 border border-stone-800 rounded-xl transition-all"
 		in:scale={{ duration: 150 }}
 	>
-		<SectionTitle text="Registrar Cliente" />
-		<input bind:value={name} type="text" class="input" placeholder="Nombre" />
-		<input bind:value={email} type="text" class="input" placeholder="Correo" />
-		<input bind:value={phone} type="text" class="input" placeholder="Teléfono" />
-		{#if inputError}
-			<InputError text={errorMessage} />
-		{/if}
-		<div class="grid grid-cols-2 gap-4">
-			<button class="btn variant-ringed-primary" on:click={cancelHandler}>Cancelar</button>
-			<button class="btn variant-filled-primary font-bold" on:click={toggleConfirmation}>
-				Confirmar
-			</button>
+		<SectionTitle centered={true} text={editing ? 'Editar Cliente' : 'Registrar Cliente'} />
+		<div class="space-y-4">
+			<div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+				<div class="input-group-shim"><Icon icon={userOutline} height={24} /></div>
+				<input bind:value={name} type="text" class="input" placeholder="Nombre" />
+			</div>
+			<div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+				<div class="input-group-shim"><Icon icon={letterOutline} height={24} /></div>
+				<input bind:value={email} type="text" class="input" placeholder="Correo" />
+			</div>
+			<div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+				<div class="input-group-shim"><Icon icon={phoneCallingRoundedOutline} height={24} /></div>
+				<input bind:value={phone} type="text" class="input" placeholder="Teléfono" />
+			</div>
+			{#if inputError}
+				<InputError text={errorMessage} />
+			{/if}
+			<div class="grid grid-cols-2 gap-4">
+				<button class="btn variant-ringed-primary" on:click={cancelHandler}>Cancelar</button>
+				<button class="btn variant-filled-primary font-bold" on:click={toggleConfirmation}>
+					Confirmar
+				</button>
+			</div>
 		</div>
 	</div>
 {/if}
