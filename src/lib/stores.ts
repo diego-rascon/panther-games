46232c0aed19a443781b175f2740d1a31feffde1
DiegoRascon <joserascon2002@gmail.com>
2023-05-04 23:42:19 -0700

    Se agregó la store de clientes

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -2,3 +2,4 @@ import { writable } from 'svelte/store';
 
 export const productsStore = writable<{ [key: string]: number | string | boolean }[]>([]);
 export const cartStore = writable<{ [key: string]: number | string | boolean }[]>([]);
+export const clientsStore = writable<{ [key: string]: number | string | boolean }[]>([]);
