0c44265e02aec05d24e5d914c6526ae09275881d
DiegoRascon <joserascon2002@gmail.com>
2023-05-06 14:19:04 -0700

    Se agregó una store que almacena las ventas

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -2,4 +2,5 @@ import { writable } from 'svelte/store';
 
 export const productsStore = writable<{ [key: string]: number | string | boolean }[]>([]);
 export const cartStore = writable<{ [key: string]: number | string | boolean }[]>([]);
+export const salesStore = writable<{ [key: string]: number | string | boolean }[]>([]);
 export const clientsStore = writable<{ [key: string]: number | string | boolean }[]>([]);
