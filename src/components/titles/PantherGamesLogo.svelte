785d3d17e0c68b34e3091c456ee26b4792a7c4e7
DiegoRascon <joserascon2002@gmail.com>
2023-05-05 23:27:29 -0700

    Ahora se usa la variante primaria

--- src/components/titles/PantherGamesLogo.svelte
+++ src/components/titles/PantherGamesLogo.svelte
@@ -2,7 +2,9 @@
 	export let size: Number;
 </script>
 
-<div class="unstyled flex flex-col text-center -space-y-2 text-{size}xl font-lexend text-pink-700 hover:text-pink-600 active:text-pink-800 transition-all">
+<div
+	class="unstyled flex flex-col text-center -space-y-2 text-{size}xl font-lexend text-primary-500 select-none"
+>
 	<span class="font-bold">panther</span>
 	<span>games</span>
 </div>
