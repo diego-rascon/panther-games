beeae80ac4de5cc0b2e6d40193f4b1eab0f9ad66
DiegoRascon <joserascon2002@gmail.com>
2023-05-06 17:13:13 -0700

    Se reformateó el código

--- src/routes/home/sales/+page.server.ts
+++ src/routes/home/sales/+page.server.ts
@@ -2,10 +2,7 @@ import { supabase } from '$lib/db';
 
 export const load = async () => {
 	const fetchSales = async () => {
-		const { data } = await supabase
-			.from('venta')
-			.select()
-			.order('venta_id', { ascending: false });
+		const { data } = await supabase.from('venta').select().order('venta_id', { ascending: false });
 		return data ?? [];
 	};
 	return { sales: fetchSales() };
