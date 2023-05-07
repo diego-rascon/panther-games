351d78358ee9ec5bd923633cb074b3b891f95642
DiegoRascon <joserascon2002@gmail.com>
2023-05-06 17:32:35 -0700

    Ahora se muestran los datos de la venta correctamente

--- src/components/SaleRow.svelte
+++ src/components/SaleRow.svelte
@@ -1,23 +1,73 @@
 <script lang="ts">
+	import { salesStore } from '$lib/stores';
+	import { popup } from '@skeletonlabs/skeleton';
+	import Dropdown from './dropdown/Dropdown.svelte';
+	import DropdownItem from './dropdown/DropdownItem.svelte';
 	import Icon from '@iconify/svelte';
 	import menuDotsBold from '@iconify/icons-solar/menu-dots-bold';
+	import trashBinMinimalisticLinear from '@iconify/icons-solar/trash-bin-minimalistic-linear';
+	import userCheckOutline from '@iconify/icons-solar/user-check-outline';
 
+	export let toggleSale: (saleId: number) => void = () => {};
 	export let id: number;
-	export let total: number;
-	export let discount: number;
-	export let date: string;
+
+	$: sale = $salesStore.find((saleEntry: any) => saleEntry.venta_id === id);
+	$: originalDate = new Date(String(sale?.venta_fecha));
+	$: formattedDate = originalDate.toLocaleDateString('en-GB');
+	$: total = sale?.venta_monto;
+	$: discount = sale?.venta_descuento;
+	$: paymentType = sale?.venta_tarjeta ? 'Tarjeta' : 'Efectivo';
+	$: client = sale?.cliente_id;
+	$: active = sale?.venta_activa;
+
+	let selected = false;
+
+	let dropdown: any = {
+		placement: 'top-end',
+		event: 'focus-click',
+		state: () => (selected = !selected)
+	};
 </script>
 
-<tr>
-	<td class="p-2 text-left">{id}</td>
-	<td class="p-2 text-left">{total}</td>
-	<td class="p-2 text-left">{discount}</td>
-	<td class="p-2 text-left">{date}</td>
-	<td class="py-2 text-right">
+<tr
+	class="border-t border-stone-800 transition-all {selected
+		? 'variant-soft-primary'
+		: 'hover:bg-stone-800'}"
+>
+	<td class="p-4 text-left select-text">{id}</td>
+	<td class="text-left select-text">{formattedDate}</td>
+	<td class="text-left select-text">$ {total}</td>
+	<td class="text-left select-text">{discount}%</td>
+	<td class="text-left select-text">{paymentType}</td>
+	<td class="text-left select-text">{client}</td>
+	<td class="pr-4 text-right">
 		<button
-			class="rounded-full p-2 hover:bg-stone-800 active:bg-stone-950 transition-all"
+			on:click
+			use:popup={{ ...dropdown, target: `dropdown-${id}` }}
+			class="btn hover:variant-filled-surface activea:variant-filled-surface p-1 rounded-full"
 		>
-			<Icon icon={menuDotsBold} rotate={1} height={24} />
+			<Icon icon={menuDotsBold} rotate={1} height={20} />
 		</button>
 	</td>
+	<div data-popup={`dropdown-${id}`}>
+		<Dropdown>
+			{#if active}
+				<DropdownItem
+					text="Eliminar"
+					icon={trashBinMinimalisticLinear}
+					on:click={() => {
+						toggleSale(id);
+					}}
+				/>
+			{:else}
+				<DropdownItem
+					text="Activar"
+					icon={userCheckOutline}
+					on:click={() => {
+						toggleSale(id);
+					}}
+				/>
+			{/if}
+		</Dropdown>
+	</div>
 </tr>
