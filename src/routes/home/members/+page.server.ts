63823c63faf257f2c39392dbf7728557aa6b7a0d
DiegoRascon <joserascon2002@gmail.com>
2023-05-07 22:12:36 -0700

    Se llama la función que regresa los datos de los clientes que son miembros

--- src/routes/home/members/+page.server.ts
+++ src/routes/home/members/+page.server.ts
@@ -2,8 +2,11 @@ import { supabase } from '$lib/db';
 
 export const load = async () => {
 	const fetchMembers = async () => {
-		const { data } = await supabase.rpc('clients_members');
+		const { data } = await supabase
+			.rpc('get_clients_members')
+			.order('miembro_id', { ascending: false });
 		return data ?? [];
 	};
+
 	return { members: fetchMembers() };
 };
