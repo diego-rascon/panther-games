6ff6bb1ed5e8314a75ff69f73aa88e98e8525e5a
DiegoRascon <joserascon2002@gmail.com>
2023-05-07 22:31:38 -0700

    Ahora se llama la función get_clients

--- src/routes/home/clients/+page.server.ts
+++ src/routes/home/clients/+page.server.ts
@@ -2,11 +2,9 @@ import { supabase } from '$lib/db';
 
 export const load = async () => {
 	const fetchClients = async () => {
-		const { data } = await supabase
-			.from('cliente')
-			.select()
-			.order('cliente_id', { ascending: false });
+		const { data } = await supabase.rpc('get_clients').order('cliente_id', { ascending: false });
 		return data ?? [];
 	};
+	
 	return { clients: fetchClients() };
 };
