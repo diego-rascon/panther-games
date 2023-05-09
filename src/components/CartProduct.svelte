ee074d22f551f89e260d126269ee161011984275
DiegoRascon <joserascon2002@gmail.com>
2023-05-08 20:47:18 -0700

    No se muestra el selector de cantidad cuando se realiza una renta

--- src/components/CartProduct.svelte
+++ src/components/CartProduct.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
 	import Icon from '@iconify/svelte';
 	import trashBinMinimalisticLinear from '@iconify/icons-solar/trash-bin-minimalistic-linear';
+	import type { ToastSettings } from '@skeletonlabs/skeleton';
 
 	export let removeHandler: (cartId: number, productId: number) => void;
 	export let quantityHandler: (cartId: number, quantity: number) => void;
+	export let rent: boolean;
 	export let cartId: number;
 	export let productId: number;
+	export let categoryId: number;
+	export let categoryName: string;
+	export let platformName: string;
 	export let name: string;
+	export let newProduct: boolean;
 	export let price: number;
 	export let stock: number;
 	export let quantity: number;
@@ -20,23 +26,36 @@
 <div class="p-4 bg-stone-800 rounded-xl space-y-2">
 	<div class="flex justify-between">
 		<p class="line-clamp-2 w-32">{name}</p>
-		<p class="mr-1 flex-shrink-0">{formattedPrice}</p>
+		<p class="mr-2 flex-shrink-0">{formattedPrice}</p>
+	</div>
+	<div class="flex flex-col space-y-1 items-start">
+		<div class="flex space-x-2">
+			<p class="unstyled px-2 p-1 variant-soft-primary rounded-full text-xs">{categoryName}</p>
+			{#if !newProduct}
+				<p class="unstyled px-2 p-1 variant-soft-warning rounded-full text-xs">Usado</p>
+			{/if}
+		</div>
+		{#if categoryId === 1}
+			<p class="unstyled flex px-2 p-1 variant-soft-success rounded-full text-xs">{platformName}</p>
+		{/if}
 	</div>
 	<div class="flex justify-end space-x-2">
-		<input
-			type="number"
-			class="input w-16"
-			min="1"
-			max={stock}
-			bind:value={quantity}
-			on:change={() => {
-				if (quantity > stock) quantity = stock;
-				quantityHandler(cartId, quantity);
-			}}
-		/>
+		{#if !rent}
+			<input
+				type="number"
+				class="input h-9 text-sm w-16 variant-filled-surface"
+				min="1"
+				max={stock}
+				bind:value={quantity}
+				on:change={() => {
+					if (quantity > stock) quantity = stock;
+					quantityHandler(cartId, quantity);
+				}}
+			/>
+		{/if}
 		<button
 			type="button"
-			class="btn-icon hover:variant-soft"
+			class="btn variant-filled-surface p-2 rounded-full"
 			on:click={() => {
 				removeHandler(cartId, productId);
 			}}
