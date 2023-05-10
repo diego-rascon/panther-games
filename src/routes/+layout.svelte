bed220f3dbd5644ce282b7fd8eed1c8c5382b614
DiegoRascon <joserascon2002@gmail.com>
2023-05-10 16:51:03 -0700

    Se importan los componentes necesarios para el dropdown

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	import '../theme.postcss';
 	import '@skeletonlabs/skeleton/styles/all.css';
-	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	import { Toast } from '@skeletonlabs/skeleton';
 
-	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+	storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
 </script>
 
 <Toast />
