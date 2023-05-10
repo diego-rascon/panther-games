0570a4f3a08de98ff6771860384d47b36763f4f2
DiegoRascon <joserascon2002@gmail.com>
2023-05-10 00:45:41 -0700

    Ahora se ordena por fecha

--- src/routes/home/cash/+page.server.ts
+++ src/routes/home/cash/+page.server.ts
@@ -2,7 +2,7 @@ import { supabase } from '$lib/db';
 
 export const load = async () => {
 	const fetchCaja = async () => {
-		const { data } = await supabase.from('caja').select().order('caja_id', { ascending: false });
+		const { data } = await supabase.from('caja').select().order('caja_fecha', { ascending: false });
 		return data ?? [];
 	};
 	return { caja: fetchCaja() };
