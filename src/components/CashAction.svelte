ce4b4f5b9afc03b89705a2da564ca551ed35cfbe
DiegoRascon <joserascon2002@gmail.com>
2023-05-09 21:14:27 -0700

    Componente que muesta las posibles acciones de caja

---
+++ src/components/CashAction.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import Icon from '@iconify/svelte';
+	import type { IconifyIcon } from '@iconify/svelte';
+
+	export let icon: IconifyIcon;
+	export let text: string;
+	export let active: boolean;
+</script>
+
+<button
+	class="flex flex-col p-4 btn {active ? 'variant-filled-primary' : 'variant-soft-primary'}"
+	on:click
+>
+	<div class="flex flex-col space-y-2 items-center">
+		<Icon icon={icon} height={48} />
+		<p class="unstyled font-lexend font-bold text-2xl">{text}</p>
+	</div>
+</button>
