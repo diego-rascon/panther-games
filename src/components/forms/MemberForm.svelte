a8721c5df4ad92d3f0a231af8dfc075c124823d7
DiegoRascon <joserascon2002@gmail.com>
2023-05-09 18:15:58 -0700

    Ahora el formulario cambia dependiendo de si se está realizando una edición o un registro

--- src/components/forms/MemberForm.svelte
+++ src/components/forms/MemberForm.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-	import { fade, scale } from 'svelte/transition';
+	import { scale } from 'svelte/transition';
 	import SectionTitle from '../titles/SectionTitle.svelte';
+	import SectionSubtitle from '../titles/SectionSubtitle.svelte';
 	import ConfirmDialog from '../modals/ConfirmDialog.svelte';
 	import InputError from '../utils/InputError.svelte';
 	import Icon from '@iconify/svelte';
@@ -8,6 +9,8 @@
 	import letterOutline from '@iconify/icons-solar/letter-outline';
 	import phoneCallingRoundedOutline from '@iconify/icons-solar/phone-calling-rounded-outline';
 	import calendarOutline from '@iconify/icons-solar/calendar-outline';
+	import wadOfMoneyOutline from '@iconify/icons-solar/wad-of-money-outline';
+	import cashOutOutline from '@iconify/icons-solar/cash-out-outline';
 	import { RadioGroup, RadioItem } from '@skeletonlabs/skeleton';
 
 	export let editing = false;
@@ -21,12 +24,17 @@
 
 	let newClient = false;
 	let clientId: number;
+	let cashPayment = true;
+	let payment: number;
+	let total = 300;
 	let inputError = false;
 	let errorMessage: string;
 
 	const namePattern = /^[a-zA-ZáéíóúñÁÉÍÓÚÑ\s]*$/;
 	const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 	const phonePattern = /^\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$/;
+
+	$: formattedTotal = total.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
 
 	const validInput = (): boolean => {
 		if (!name || !namePattern.test(name)) {
@@ -75,11 +83,15 @@
 	>
 		<SectionTitle centered={true} text={editing ? 'Editar Miembro' : 'Registrar Miembro'} />
 		<div class="flex flex-col space-y-4">
-			<RadioGroup class="justify-center" active="variant-filled-primary">
-				<RadioItem bind:group={newClient} name="justify" value={true}>Nuevo cliente</RadioItem>
-				<RadioItem bind:group={newClient} name="justify" value={false}>Cliente existente</RadioItem>
-			</RadioGroup>
-			{#if newClient}
+			{#if !editing}
+				<RadioGroup class="justify-center" active="variant-filled-primary">
+					<RadioItem bind:group={newClient} name="justify" value={true}>Nuevo cliente</RadioItem>
+					<RadioItem bind:group={newClient} name="justify" value={false}
+						>Cliente existente</RadioItem
+					>
+				</RadioGroup>
+			{/if}
+			{#if newClient || editing}
 				<div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
 					<div class="input-group-shim"><Icon icon={userOutline} height={24} /></div>
 					<input bind:value={name} type="text" class="input" placeholder="Nombre" />
@@ -102,10 +114,46 @@
 					</select>
 				</div>
 			{/if}
-			<div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
-				<div class="input-group-shim"><Icon icon={calendarOutline} height={24} /></div>
-				<input bind:value={startDate} type="date" class="input" placeholder="Fecha inicial" />
-			</div>
+			{#if !editing}
+				<SectionSubtitle text="Fecha inicial" />
+				<div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+					<div class="input-group-shim"><Icon icon={calendarOutline} height={24} /></div>
+					<input bind:value={startDate} type="date" class="input" placeholder="Fecha inicial" />
+				</div>
+			{/if}
+			{#if !editing}
+				<SectionSubtitle text="Pago" />
+				<RadioGroup class="justify-center" active="variant-filled-primary">
+					<RadioItem bind:group={cashPayment} name="justify" value={true}>Efectivo</RadioItem>
+					<RadioItem bind:group={cashPayment} name="justify" value={false}>Tarjeta</RadioItem>
+				</RadioGroup>
+				{#if cashPayment}
+					<div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+						<div class="input-group-shim"><Icon icon={wadOfMoneyOutline} height={24} /></div>
+						<input
+							bind:value={payment}
+							type="number"
+							min="0"
+							class="input"
+							placeholder="Cantidad"
+						/>
+					</div>
+					<div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+						<div class="input-group-shim"><Icon icon={cashOutOutline} height={24} /></div>
+						<input
+							class="input"
+							type="number"
+							placeholder="Cambio"
+							value={payment - total < 0 ? null : payment - total}
+							readonly
+						/>
+					</div>
+				{/if}
+				<div class="flex justify-between text-lg">
+					<p class="unstyled font-bold">Total</p>
+					<p class="unstyled">{formattedTotal}</p>
+				</div>
+			{/if}
 			{#if inputError}
 				<InputError text={errorMessage} />
 			{/if}
