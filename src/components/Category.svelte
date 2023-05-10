0ca57fd92dead966f4b1339b2310334a39aa6c9e
DiegoRascon <joserascon2002@gmail.com>
2023-05-09 22:49:23 -0700

    Ahora cambia el colór del botón dependiendo en si la categoría está activa o no

--- src/components/Category.svelte
+++ src/components/Category.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
+	export let active: boolean;
 	export let text: string;
 </script>
 
-<button on:click class="btn variant-soft-primary p-4 unstyled font-lexend font-bold text-xl">
+<button
+	on:click
+	class="btn p-4 unstyled font-lexend font-bold text-xl {active
+		? 'variant-filled-primary'
+		: 'variant-soft-primary'}"
+>
 	{text}
 </button>
