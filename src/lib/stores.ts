ce66a3029ceb806e030c92b340d4665edcb28a7c
DiegoRascon <joserascon2002@gmail.com>
2023-05-13 17:10:21 -0700

    Se agregaron stores para manejar el login

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,5 +1,9 @@
 import { writable } from 'svelte/store';
 
+export const session = writable(false);
+export const readUser = writable(false);
+export const editUser = writable(false);
+
 export const productsStore = writable<{ [key: string]: number | string | boolean }[]>([]);
 export const cartStore = writable<{ [key: string]: number | string | boolean }[]>([]);
 export const salesStore = writable<{ [key: string]: number | string | boolean }[]>([]);
