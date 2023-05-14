22390cdddfd05a22fca02cceb12fd65aa3cb4d53
DiegoRascon <joserascon2002@gmail.com>
2023-05-13 17:11:27 -0700

    Se redirecciona a la pantalla de iniciar sesión cuando no se tiene una sesión activa

--- src/routes/home/+layout.svelte
+++ src/routes/home/+layout.svelte
@@ -1,10 +1,16 @@
 <script>
+	import { goto } from '$app/navigation';
+	import { session } from '$lib/stores';
 	import Navbar from '../../components/navigation/Navbar.svelte';
+
+	if (!$session) goto('/');
 </script>
 
 <div class="min-h-screen bg-stone-950">
-	<Navbar />
-	<main class="flex-1 p-4 ml-64 select-none">
-		<slot />
-	</main>
+	{#if $session}
+		<Navbar />
+		<main class="flex-1 p-4 ml-64 select-none">
+			<slot />
+		</main>
+	{/if}
 </div>
