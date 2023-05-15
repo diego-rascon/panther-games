a1ec28cbf483eb1397f0438e91ba79105646407a
DiegoRascon <joserascon2002@gmail.com>
2023-05-14 19:34:00 -0700

    Se agregó una prop para reflejar el ícono verticalmente

--- src/components/Dropdown/DropdownItem.svelte
+++ src/components/Dropdown/DropdownItem.svelte
@@ -4,9 +4,10 @@
 
 	export let text: string;
 	export let icon: IconifyIcon;
+	export let flipIcon = false;
 </script>
 
 <button on:click|stopPropagation class="btn variant-filled-surface justify-start space-x-4">
-	<Icon {icon} height={20} />
+	<Icon {icon} height={20} hFlip={flipIcon} />
 	<p>{text}</p>
 </button>
